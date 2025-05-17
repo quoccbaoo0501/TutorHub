fa1b19e93734c726e3412d8ede3740ad34ce5510
Kevin <quoccbaoo0501@gmail.com>
2025-05-17 19:36:22 +0700

    remove unused loading pages

--- app/login/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="flex min-h-screen items-center justify-center">
-      <div className="h-12 w-12 animate-spin rounded-full border-4 border-solid border-primary border-t-transparent"></div>
-    </div>
-  );
-}
